817031e552efd22ac626d754dc84c182b77e6fbc
Joel Mickelson <joelmickelson@hotmail.com>
2021-01-29 21:40:08 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParsingViaCKY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
